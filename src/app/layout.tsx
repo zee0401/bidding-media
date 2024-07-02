005d71b99adf4e4c25ab3701fb92791e86ed66fb
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-07-02 16:24:08 +0530

    shadcn added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Header } from "@/app/header";
+import { SessionProvider } from "next-auth/react";
+import { AppKnockProviders } from "@/app/knock-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <SessionProvider>
+          <AppKnockProviders>
+            <Header />
+            <div className="container mx-auto py-12">{children}</div>
+          </AppKnockProviders>
+        </SessionProvider>
+      </body>
     </html>
   );
 }
